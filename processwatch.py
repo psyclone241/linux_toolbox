87ef5522f3637ca3596d5486b1f17643f3ae1db2
Roy Keyes <keyes@ufl.edu>
2016-04-20 09:55:09 -0400

    Add a printing method, so that all parts of the class print through a single interface and fixed formatting of the columns displayed while the processes are being counted

--- processwatch.py
+++ processwatch.py
@@ -8,11 +8,24 @@ import time
 import sqlite3 as db
 import getpass
 import json
+import argparse
 
 class ProcessWatch:
     def __init__(self):
-        self.config_data = None
 
+        self.arg_parser = argparse.ArgumentParser()
+        self.arg_parser.add_argument(
+            "-c", "--config_file", help="Location of the configuration file")
+        # self.arg_parser.add_argument(
+        #     "-i", "--input", help="Which part of the project to extract?", default='project')
+        # self.arg_parser.add_argument(
+        #     "-m", "--method_to_use", help="Which method to use?", default='run_command')
+        self.arg_parser.add_argument(
+            "-q", "--quiet", help="Prevents output to the screen", action="store_true", default=True)
+
+        self.args = self.arg_parser.parse_args()
+
+        self.config_data = None
         self.database_connection = None
         self.database_cursor = None
         self.process_list = None
@@ -29,24 +42,36 @@ class ProcessWatch:
             }
         }
         self.defaults = {
+            'config_file': 'config.ini',
             'max_in_list': 5,
             'cpu_percent_interval': .25,
             'database_file': 'processwatch.db'
         }
 
+        # Collect any arguments passed to argparser
+        self.quiet = self.args.quiet
+
+        # Set the default for the configuration file
+        if self.args.config_file:
+            self.config_file = self.args.config_file
+        else:
+            self.config_file = self.defaults['config_file']
+
+        # Try to load all the configuration data from the config file
         try:
             config = ConfigParser.ConfigParser()
-            if os.path.exists('config.ini'):
-                config.read("config.ini")
+            if os.path.exists(self.config_file):
+                config.read(self.config_file)
                 self.config_data = {}
                 self.config_data['database_file'] = config.get('GeneralSettings', 'DatabaseFile')
                 self.config_data['max_in_list'] = config.get('GeneralSettings', 'MaxInList')
                 self.config_data['cpu_percent_interval'] = config.get('GeneralSettings', 'CPUPercentInterval')
             else:
-                raise Exception('No config.ini file was found')
+                raise Exception('Specified config file: [' + self.config_file + '] was found')
         except Exception, e:
-            print('Could not load config.ini')
-            print(e)
+            self.sendToTerminal('Could not load ' + self.config_file)
+            self.sendToTerminal(e)
+            self.sendToTerminal('Running with defaults')
         finally:
             # Define variables for use later in the class
 
@@ -88,19 +113,24 @@ class ProcessWatch:
             self.abs_round_memory_format = '%3.1f%s%s'
             self.other_round_memory_format = '%.1f%s%s'
             self.timestamp_format = '%Y-%m-%d %H:%M:%S'
-            self.display_column_format = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'
+            # self.display_column_format = '%s\t%s\t%s\t%s\t%s\t%s\t%s\t%s'
+            self.display_column_format = '{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}\t{:s}'
             self.display_pid_justification = 6
             self.display_user_justification = 15
             self.display_name_justification = 20
             self.display_rsscpu_justification = 10
 
     def main(self):
+        # Run the series of methods used to build and publish the process list
         self.databaseSetup()
         self.getProcessList()
         self.readProcessList()
         self.scoreProcessList()
 
-    def sizeof_fmt(self, num, suffix='b'):
+    def sendToTerminal(self, message):
+        print(message)
+
+    def sizeOf(self, num, suffix='b'):
         try:
             for unit in ['','k','m','g','t','p','e','z']:
                 if abs(num) < 1024.0:
@@ -121,17 +151,19 @@ class ProcessWatch:
             error_message = e.args[0]
 
             if 'no such table' in error_message:
-                print('No process data storage table in DB, creating table "' + self.process_table + '"')
+                self.sendToTerminal('No process data storage table in DB, creating table "' + self.process_table + '"')
                 try:
                     self.database_cursor.execute('CREATE TABLE ' + self.process_table + ' ' + self.create_table_columns)
                     self.database_cursor.execute('SELECT * from ' + self.process_table)
                     process_data = self.database_cursor.fetchall()
                 except db.Error, e:
                     error_message = e.args[0]
-                    print "Error in DB: %s" % error_message
+                    screen_message = "Error in DB: {:s}".format((error_message))
+                    self.sendToTerminal(screen_message)
                     exit()
             else:
-                print "Error in DB: %s" % error_message
+                    screen_message = "Error in DB: {:s}".format((error_message))
+                    self.sendToTerminal(screen_message)
                 exit()
         finally:
             if self.database_connection:
@@ -155,64 +187,72 @@ class ProcessWatch:
                         if pinfo['memory_info']:
                             self.scorecard.append((pinfo['pid'], pinfo['name'], pinfo['memory_info'][0]))
         else:
-            print('No processes were collected')
+            self.sendToTerminal('No processes were collected')
 
     def scoreProcessList(self):
 
-        self.database_connection = db.connect(self.database_file, isolation_level=None)
-        self.database_cursor = self.database_connection.cursor()
+        try:
+            self.database_connection = db.connect(self.database_file, isolation_level=None)
+            self.database_cursor = self.database_connection.cursor()
 
-        # Setup the columns for printing on screen results
-        print(self.display_column_format % ('Rank', 'PID'.ljust(self.display_pid_justification), 'PPID'.ljust(self.display_pid_justification), 'CUser'.ljust(self.display_user_justification), 'PUser'.ljust(self.display_user_justification), 'Name'.ljust(self.display_name_justification), 'RSS'.ljust(self.display_rsscpu_justification), 'CPU%'.ljust(self.display_rsscpu_justification)))
+            # Setup the columns for printing on screen results
+            header_column = self.display_column_format.format('Rank', 'PID'.ljust(self.display_pid_justification), 'PPID'.ljust(self.display_pid_justification), 'CUser'.ljust(self.display_user_justification), 'PUser'.ljust(self.display_user_justification), 'Name'.ljust(self.display_name_justification), 'RSS'.ljust(self.display_rsscpu_justification), 'CPU%'.ljust(self.display_rsscpu_justification))
+            self.sendToTerminal(header_column)
 
-        # Sort the scorecards from the readProcessList returned value
-        self.scorecard = sorted(self.scorecard, key=lambda x: x[2], reverse=True)
+            # Sort the scorecards from the readProcessList returned value
+            self.scorecard = sorted(self.scorecard, key=lambda x: x[2], reverse=True)
 
-        # Start the ranking at 1, because it would be silly not to
-        rank = 1
-        # Set a single timestamp for each run that is stored in the database
-        timestamp = time.time()
-        timestamp = datetime.datetime.fromtimestamp(timestamp).strftime(self.timestamp_format)
+            # Start the ranking at 1, because it would be silly not to
+            rank = 1
+            # Set a single timestamp for each run that is stored in the database
+            timestamp = time.time()
+            timestamp = datetime.datetime.fromtimestamp(timestamp).strftime(self.timestamp_format)
 
-        # Leave this loop open so that we could in theory handle things outside
-        # of the max_in_list variable set in configuration
-        for process in self.scorecard:
-            if rank <= self.max_in_list:
-                pid = process[0]
-                name = process[1]
-                rss = process[2]
+            # Leave this loop open so that we could in theory handle things outside
+            # of the max_in_list variable set in configuration
+            for process in self.scorecard:
+                if rank <= self.max_in_list:
+                    pid = process[0]
+                    name = process[1]
+                    rss = process[2]
 
-                try:
-                    # First get the details of the process directly from the system
-                    process_info = psutil.Process(pid)
-                    process_info = process_info.as_dict()
-                    process_user = process_info['username']
-                    ppid = process_info['ppid']
-                    command_line = json.dumps(process_info['cmdline'])
-                    create_time = datetime.datetime.fromtimestamp(process_info['create_time']).strftime(self.timestamp_format)
+                    try:
+                        # First get the details of the process directly from the system
+                        process_info = psutil.Process(pid)
+                        process_info = process_info.as_dict()
+                        process_user = process_info['username']
+                        ppid = process_info['ppid']
+                        command_line = json.dumps(process_info['cmdline'])
+                        create_time = datetime.datetime.fromtimestamp(process_info['create_time']).strftime(self.timestamp_format)
 
-                    # Independently grab the process a second time so as to get
-                    # an accurate reading of the CPU%
-                    # Read -> https://pythonhosted.org/psutil/#psutil.Process.cpu_percent
-                    process_info = psutil.Process(pid)
-                    cpu_percent = process_info.cpu_percent(interval=self.cpu_percent_interval)
-                    current_user = getpass.getuser()
+                        # Independently grab the process a second time so as to get
+                        # an accurate reading of the CPU%
+                        # Read -> https://pythonhosted.org/psutil/#psutil.Process.cpu_percent
+                        process_info = psutil.Process(pid)
+                        cpu_percent = process_info.cpu_percent(interval=self.cpu_percent_interval)
+                        current_user = getpass.getuser()
 
-                    # Print each row of data
-                    print(self.display_column_format % (str(rank), str(pid).ljust(self.display_pid_justification), str(ppid).ljust(self.display_pid_justification), current_user.ljust(self.display_user_justification), process_user.ljust(self.display_user_justification), name.ljust(self.display_name_justification), str(self.sizeof_fmt(rss)).ljust(self.display_rsscpu_justification), str(cpu_percent).ljust(self.display_rsscpu_justification)))
+                        # Print each row of data
+                        row = self.display_column_format.format(str(rank), str(pid).ljust(self.display_pid_justification), str(ppid).ljust(self.display_pid_justification), current_user.ljust(self.display_user_justification), process_user.ljust(self.display_user_justification), name.ljust(self.display_name_justification), str(self.sizeOf(rss)).ljust(self.display_rsscpu_justification), str(cpu_percent).ljust(self.display_rsscpu_justification))
+                        self.sendToTerminal(row)
 
-                    # Run an insert query to store this row in the database
-                    data = (rank, pid, ppid, current_user, process_user, name, rss, cpu_percent, command_line, timestamp, create_time)
-                    self.database_cursor.execute("INSERT INTO " + self.process_table + "(rank, pid, ppid, run_as, username, name, rss, cpu_percent, cmdline, timestamp, create_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
-                except psutil.NoSuchProcess:
-                    pass
+                        # Run an insert query to store this row in the database
+                        data = (rank, pid, ppid, current_user, process_user, name, rss, cpu_percent, command_line, timestamp, create_time)
+                        self.database_cursor.execute("INSERT INTO " + self.process_table + "(rank, pid, ppid, run_as, username, name, rss, cpu_percent, cmdline, timestamp, create_time) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", data)
+                    except psutil.NoSuchProcess:
+                        pass
 
-            # Increment the ranking variable by one
-            # This is used to mark the record in the database with a score
-            # for use in determining the processes rank at the time of the test
-            rank += 1
+                # Increment the ranking variable by one
+                # This is used to mark the record in the database with a score
+                # for use in determining the processes rank at the time of the test
+                rank += 1
 
-        self.database_connection.close()
+        except db.Error, e:
+            error_message = e.args[0]
+            self.sendToTerminal(error_message)
+        finally:
+            if self.database_connection:
+                self.database_connection.close()
 
 debug = True
 if __name__ == '__main__':
