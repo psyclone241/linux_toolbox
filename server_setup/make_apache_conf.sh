ec974a7bee4c390c16dc1356ca48ffee034d5e0e
Roy Keyes <keyes@ufl.edu>
2016-09-21 17:36:39 -0400

    Rework make_apache_conf script to use the domain and then subdomains in order to make a primary or subdomain record

--- server_setup/make_apache_conf.sh
+++ server_setup/make_apache_conf.sh
@@ -9,8 +9,8 @@ OUTPUT_PATH=${3}
 TEMPLATE_FILE=template_conf
 FILE_EXTENSION=conf
 
-IFS=',' read -r -a sub_domains <<< ${SUB_DOMAIN_NAMES}
-IFS='. ' read -r -a is_domain_primary <<< ${DOMAIN_NAME}
+IFS=', ' read -r -a sub_domains <<< ${SUB_DOMAIN_NAMES}
+# IFS='. ' read -r -a is_domain_primary <<< ${DOMAIN_NAME}
 
 function makeNewFile {
     if [ ! -z ${OUTPUT_PATH} ];
@@ -43,34 +43,43 @@ if [ -z "${DOMAIN_NAME}" ];
 then
     echo "No domain name specified"
 else
-    new_file_name=${DOMAIN_NAME}.${FILE_EXTENSION}
-    if [ "${#is_domain_primary[@]}" -gt 2 ];
+    if [ ! -z ${SUB_DOMAIN_NAMES} ];
     then
         echo "Making a subdomain"
+        first_sub_domain=${sub_domains[0]}
+        new_domain_name=${first_sub_domain}.${DOMAIN_NAME}
+        new_file_name=${new_domain_name}.${FILE_EXTENSION}
         makeNewFile ${new_file_name} ${TEMPLATE_FILE}
 
-        SITE_DIRECTORY=${is_domain_primary[0]}
+        SERVER_NAME=${new_domain_name}
+        SITE_DIRECTORY=${first_sub_domain}
 
-        if [ -z "${SUB_DOMAIN_NAMES}" ];
+        if [ "${#sub_domains[@]}" -gt 1 ];
         then
-            SERVER_ALIAS="#ServerAlias"
-        else
             SERVER_ALIAS="ServerAlias"
             echo "Sub Domain Names: ${SUB_DOMAIN_NAMES}"
+            counter=0
             for sub_domain in "${!sub_domains[@]}";
             do
                 sub_domain_index=${sub_domain}
                 sub_domain_name=${sub_domains[sub_domain]}
-
-                SERVER_ALIAS=${SERVER_ALIAS}" "${sub_domain_name}"."${DOMAIN_NAME}
+                if [ ${counter} -gt 0 ];
+                then
+                    SERVER_ALIAS=${SERVER_ALIAS}" "${sub_domain_name}"."${DOMAIN_NAME}
+                fi
+                let counter=counter+1
             done
+        else
+            SERVER_ALIAS="#ServerAlias"
         fi
 
         GO=1
     else
+        new_file_name=${DOMAIN_NAME}.${FILE_EXTENSION}
         echo "Making Primary: ${DOMAIN_NAME}"
         makeNewFile ${new_file_name} ${TEMPLATE_FILE}
 
+        SERVER_NAME=${DOMAIN_NAME}
         SERVER_ALIAS="ServerAlias www.${DOMAIN_NAME}"
         if [ ! -z "${SUB_DOMAIN_NAMES}" ];
         then
@@ -91,7 +100,8 @@ else
     if [ ${GO} -gt 0 ];
     then
         replaceConfVariable "SERVER_ADMIN" "${SERVER_ADMIN}" ${new_file_name}
-        replaceConfVariable "SERVER_NAME" "${DOMAIN_NAME}" ${new_file_name}
+        replaceConfVariable "SERVER_NAME" "${SERVER_NAME}" ${new_file_name}
+        replaceConfVariable "DOMAIN_NAME" "${DOMAIN_NAME}" ${new_file_name}
         replaceConfVariable "SERVER_ALIAS" "${SERVER_ALIAS}" ${new_file_name}
         replaceConfVariable "DOCUMENT_ROOT" "${DOCUMENT_ROOT}" ${new_file_name}
         replaceConfVariable "SITE_DIRECTORY" "${SITE_DIRECTORY}" ${new_file_name}
